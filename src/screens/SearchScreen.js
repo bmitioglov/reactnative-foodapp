c437568558975b87fa9a204679eda73731930040
Boris Mitioglov <boris_mitioglov@epam.com>
2020-10-27 17:11:27 -0700

    yelp api added

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet} from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
 
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+    const searchApi = async searchTerm => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san diego'
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (e) {
+            setErrorMessage('Something went wrong');
+        }
+    };
+
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
 
     return <View>
         <SearchBar
             term={term}
-            onTermChange={(newTerm) => setTerm(newTerm)}
-            onTermSubmit={() => console.log('Term submitted')}
+            onTermChange={setTerm}
+            onTermSubmit={() => searchApi(term)}
         />
-        <Text>{term}</Text>
+        { errorMessage ? <Text>{errorMessage}</Text> : null }
+        <Text>We have found {results.length} results</Text>
     </View>
 };
 
