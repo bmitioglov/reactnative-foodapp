4700155e069941fdc2229181d80240aec08646c1
Boris Mitioglov <boris_mitioglov@epam.com>
2020-11-06 09:52:51 -0800

    ResultShowScreen.js was added

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,11 +4,14 @@ import { withNavigation } from "react-navigation";
 import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ( {title, results, navigation} ) => {
+    if (!results.length) {
+        return null;
+    }
+
     return <View style={styles.container}>
         <Text style={styles.title}>
             {title}
         </Text>
-        {/*<Text>Results: {results.length}</Text>*/}
         <FlatList
             horizontal
             showsHorizontalScrollIndicator={false}
@@ -16,7 +19,7 @@ const ResultsList = ( {title, results, navigation} ) => {
             keyExtractor={(result) => result.id}
             renderItem={({item}) => {
                 return (
-                    <TouchableOpacity onPress={() => navigation.navigate('ResultShow')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResultShow', {id: item.id})}>
                         <ResultsDetail result={item}/>
                     </TouchableOpacity>
                 );
